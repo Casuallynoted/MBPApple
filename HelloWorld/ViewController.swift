c13ea98e348d8bb26c96f10c291a5c100fae1d08
Casuallynoted <taffyman12349@gmail.com>
2015-01-13 01:41:02 -0500

    Initial Commit
    No version number, pending appliation of code and a bit of interface
    restructuring.

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ryan Thomas on 1/7/15.
+//  Copyright (c) 2015 Ryan Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func Withdraw(sender: AnyObject) {
+        
+    }
+    @IBOutlet weak var EditText: UITextField!
+    
+    @IBAction func Deposit(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
